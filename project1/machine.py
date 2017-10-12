984a23577474bd69c638b891ffe07b2a549dbc9f
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-10-12 09:43:26 -0700

    Refactor to PEP8

--- project1/machine.py
+++ project1/machine.py
@@ -1,5 +1,4 @@
-# Python TCP Client
-import socket 
+import socket
 import sys
 from threading import Thread
 
@@ -11,26 +10,32 @@ threads = []
 tcpClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 tcpClient.connect((host, port))
 
+
 def send_messages(connection):
     while True:
-        message = raw_input("Please enter a message to send to all clients: ");
-        connection.send(message);
-        if (message == "exit"):
+        message = raw_input("Please enter a message to send to all clients: ")
+        #binary_message = bytes(message, encoding="ascii")
+        connection.send(message)
+        if message == "exit":
             connection.close()
             break
+
 
 def listen_for_messages(connection):
     while True:
         data = connection.recv(BUFFER_SIZE)
+        #data = binary_data.decode("utf-8")
         if not data: #server closed connection
             print ("Server closed connection")
             connection.close()
             break
         print ("Received data", data)
 
+
 t = Thread(target=send_messages, args=(tcpClient, ))
 threads.append(t)
 t.start()
+
 t = Thread(target=listen_for_messages, args=(tcpClient, ))
 threads.append(t)
 t.start()
--- project1/message_server.py
+++ project1/message_server.py
@@ -1,22 +1,22 @@
 import socket
 from threading import Thread 
-import sys #command line args
+import sys
 
 ###########################################################
 #
 # @Author: Albert Chen, Havard Anda Estensen
-# 
+#
 # @Description: opens a server with NUM_CLIENTS
 # when it receives a message from a client, send
 # it to all other clients
-# 
+#
 # @README:
 # Currently written in python 2, can switch to python 3
 #
-# To run the program: python server1.py <port>
+# To run the program: python message_server.py <port>
 # Create as many client threads as required by NUM_CLIENT
-# python client.py <port1>
-# python client.py <port2>
+# python machine.py <port1>
+# python machine.py <port2>
 # ...
 #
 # Start sending messages on client threads, other clients
@@ -26,25 +26,26 @@ import sys #command line args
 ###########################################################
 
 connections = []
-threads = [] #threads that create the initial socket connections
-listen_threads = [] #threads to listen to client threads, 1 thread for each client
+threads = [] # Threads that create the initial socket connections
+listen_threads = [] # Threads to listen to client threads, 1 thread for each client
 if len(sys.argv) > 1:
     port = int(sys.argv[1])
 else:
-    port = 12345 #default port if user doesn't enter one
-NUM_CLIENTS=3 #change to however many clients you want
+    port = 12345 # Default port if user doesn't enter one
+NUM_CLIENTS=3 # Change to however many clients you want
 
 BUFFER_SIZE = 1024
+
 
 def create_connection(port):
     print("connection thread created with port", port)
     tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #for some reason there's a timeout before sockets can be used again, reuseaddr fixes this problem
-    tcp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+    # For some reason there's a timeout before sockets can be used again, reuseaddr fixes this problem
+    tcp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     try:
         tcp_server_socket.bind(("localhost", port))
         tcp_server_socket.listen(1)
-        print("tcpserver calling accept on port ", port);
+        print("tcpserver calling accept on port ", port)
         conn, addr = tcp_server_socket.accept()
         print("appending to connections the following info: ", connections, addr)
         connections.append(conn)
@@ -52,37 +53,38 @@ def create_connection(port):
         print("closing setup socket (either executed properly or timed out")
         tcp_server_socket.close()
 
+
 def listen_for_messages(connection):
     print("Listening on connection", connection)
     while True:
         data = connection.recv(BUFFER_SIZE)
-        if (not data or data == "exit"): #if data is empty, it means client closed connection
-            print ("Client closed connection")
+        if not data or data == "exit": # If data is empty, it means client closed connection
+            print("Client closed connection")
             connection.close()
-            #remove client from the connection list
             connections.remove(connection)
             print("Updated connection list:", connections)
             break
 
         print("Received data", data, "from connection", connection)
-        #send the data to all the other clients
+        # Send the data to all the other clients
         for other_connection in connections:
             if other_connection != connection:
                 other_connection.send(data)
 
-for i in range(NUM_CLIENTS): #establish connections to all clients first
+
+for i in range(NUM_CLIENTS): # Establish connections to all clients first
     t = Thread(target=create_connection, args=(port+i, ))
     threads.append(t)
     t.start()
 
-for t in threads: #clean up threads
+for t in threads: # Clean up threads
     t.join()
 
-for i in range(NUM_CLIENTS): #establish a listen thread for all clients
+for i in range(NUM_CLIENTS): # Establish a listen thread for all clients
     t = Thread(target=listen_for_messages, args=(connections[i], ))
     listen_threads.append(t)
     t.start()
 
-for t in threads: #clean up threads
+for t in threads: # Clean up threads
     t.join()
 
