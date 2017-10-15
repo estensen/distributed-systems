d2608f35cc48601edd3e985f1090c16da7b2e5b1
Albert Chen <trebla01@csil.cs.ucsb.edu>
2017-10-15 12:44:14 -0700

    Completed multi-threading lamport clock and resource allocation

--- project1/machine.py
+++ project1/machine.py
@@ -1,14 +1,20 @@
 import socket
 import sys
-from threading import Thread
-
+from threading import Thread, Lock
+from random import randint
+import time #time.sleep
 
 local_time = 0
 host = "localhost"
 port = int(sys.argv[1])
+process_id = port #port is also the process id for the machine
 BUFFER_SIZE = 1024
-NUM_MACHINES = 2
+NUM_MACHINES = 4
 threads = []
+blocked_processes=[]
+want_resource = False
+want_resource_message = ""
+mutex = Lock()
 
 tcpClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 tcpClient.connect((host, port))
@@ -17,6 +23,11 @@ tcpClient.connect((host, port))
 def update_local_time(received_time):
     return max(local_time, received_time)
 
+def read_post():
+    with open("likes.txt", "r") as f:
+        current_likes = int(f.read())
+    f.close()
+    return current_likes
 
 def like_post():
     # TODO: Make sure read and write are atomic
@@ -27,56 +38,167 @@ def like_post():
         f.seek(0)
         f.truncate()
         f.write(str(new_likes))
+        print("New likes:",new_likes)
     f.close()
 
+#returns true if my process should go first
+def return_true_if_earlier(my_request_with_EOM, other_request_with_EOM):
+    my_request = my_request_with_EOM.replace("EOM","");
+    other_request = other_request_with_EOM.replace("EOM","")
+    print("Comparing my request: {}, other request: {}".format(my_request, other_request))
+    my_time = int(my_request.split(",")[1])
+    other_time = int(other_request.split(",")[1])
+    if (my_time < other_time):
+        return True
+    elif (my_time > other_time):
+        return False
+    else:
+        my_id = int(my_request.split(",")[2])
+        other_id = int(other_request.split(",")[2])
+        if (my_id < other_id):
+            return True
+        else:
+            return False
 
 def send_messages(connection):
     # Message format: "<command>,<port>,<local_time>
     while True:
-        user_input = input("Please enter a message to send to all clients:\n")
+        user_input = input("Available commands: read, like, or exit\n")
+        #Please enter a message to send to all clients:\n")
+
+        
         global local_time
+
         local_time += 1
-        message_str = user_input + ",{},{}".format(local_time, port)
+        #construct message
+        message_str = user_input + ",{},{}EOM".format(local_time, port) #split messages by EOM: end of message
         message_binary = bytes(message_str, encoding="ascii")
-        connection.send(message_binary)
-        if message_str == "exit":
+
+
+        print( "Local time:", local_time) 
+        if user_input == "test":
+            mutex.acquire()
+            try:
+                connection.send(message_binary)
+            finally:
+                mutex.release()
+            mutex.acquire()
+            try:
+                connection.send(message_binary)
+            finally:
+                mutex.release()
+
+        elif user_input == "read":
+            print ("user called", user_input)
+            print("TESTCASE CONTENT Like:%d" %read_post())
+            mutex.acquire()
+            try:
+                connection.send(message_binary)
+            finally:
+                mutex.release()
+        elif user_input == "like":
+            print ("user called", user_input)
+            mutex.acquire()
+            try:
+                connection.send(message_binary)
+            finally:
+                mutex.release()
+            global want_resource
+            global want_resource_message
+            want_resource = True 
+            want_resource_message = message_str
+        elif user_input == "exit":
+            mutex.acquire()
+            try:
+                connection.send(bytes("exit", encoding="ascii"))
+            finally:
+                mutex.release()
             connection.close()
             break
-
+        else:
+            print ("unrecognized command, please try read, like, or exit")
 
 def listen_for_messages(connection):
     acks = set()  # Can currently only handle one lock request from each machine at a time.
     while True:
         message_binary = connection.recv(BUFFER_SIZE)
         message_str = message_binary.decode("utf-8")
-        message_arr = message_str.split(",")
-        global local_time
-        local_time += 1
-        local_time = update_local_time(int(message_arr[1]))
-        print("Local time: ", local_time)
 
-        if message_arr[0] == "lock":
-            print("Machine {} wants the lock".format(message_arr[2]))
-            response_str = "ack,{},{}".format(local_time, port)
-            response_binary = bytes(response_str, encoding="ascii")
-            connection.send(response_binary)
-
-        if message_arr[0] == "ack":
-            # TODO: Don't allow like_post() to be called if an ack that was not warranted is received.
-            print("Ack received from {}".format(message_arr[2]))
-            acks.add(message_arr[1])
-
-        if not message_arr:
-            print("Server closed connection")
+        if not message_str:
             connection.close()
             break
-        print("Received message \"{}\"".format(message_str))
 
-        if len(acks) == NUM_MACHINES - 1:
-            print("Lock acquired!")
-            like_post()
-            acks = set()
+        message_list = message_str.split("EOM")
+        #print ("message list:", message_list)
 
+        for i in range(len(message_list)-1):
+            individual_message = message_list[i]
+            message_arr = individual_message.split(",")
+
+            #close the connection if server disconnects
+            if message_arr[0] == "exit":
+                connection.close()
+                break
+
+            global local_time
+            global want_resource
+            global want_resource_message
+            local_time += 1
+            local_time = update_local_time(int(message_arr[1]))
+            print("Local time: ", local_time)
+
+            if message_arr[0] == "like":
+                request_source_port = message_arr[2]
+                print("Machine {} wants the lock".format(request_source_port))
+
+                #construct return message
+                response_str = "ack {},{},{}EOM".format(request_source_port,local_time, port)
+                response_binary = bytes(response_str, encoding="ascii")
+
+                #if i want resource, check who blocks who
+                if (want_resource):
+                    my_priority = return_true_if_earlier(want_resource_message,message_str)
+                    if (my_priority): #block the other message
+                        print("MY PROCESS IS EALIER, BLOCKING")
+                        blocked_processes.append(response_binary)
+                    else:
+                        mutex.acquire()
+                        try:
+                            connection.send(response_binary)
+                        finally:
+                            mutex.release()
+                else:
+                    mutex.acquire()
+                    try:
+                        connection.send(response_binary)
+                    finally:
+                        mutex.release()
+
+            #print(message_arr)
+            if message_arr[0].split(" ")[0] == "ack":
+                # TODO: Don't allow like_post() to be called if an ack that was not warranted is received.
+                print("Ack received from {}".format(message_arr[2]))
+                acks.add(message_arr[2])
+
+            #print("Received message \"{}\"".format(message_str))
+
+            #print("acks list: {}".format(acks))
+
+            if len(acks) == NUM_MACHINES - 1:
+                print("LOCK ACQUIRED!!!")
+                like_post()
+                #reset state variables
+                acks = set()
+                want_resource = False
+                want_resource_message = ""
+                #unblock blocked processes
+                #print ("unblocking processes", blocked_processes)
+                for response_binary in blocked_processes:
+                    mutex.acquire()
+                    try:
+                        connection.send(response_binary)
+                    finally:
+                        mutex.release()
 
 t = Thread(target=send_messages, args=(tcpClient, ))
 threads.append(t)
--- project1/message_server.py
+++ project1/message_server.py
@@ -1,54 +1,95 @@
 import socket
-from threading import Thread 
+from threading import Thread, Lock 
 import sys
+import time
 
+NUM_MACHINES = 4
+BUFFER_SIZE = 1024
+
+mutexes = []
+for i in range(NUM_MACHINES):
+    mutexes.append(Lock())
+#mutex = Lock()
 
 connections = []
+for i in range(NUM_MACHINES):
+    connections.append(0)
 threads = [] # Threads that create the initial socket connections
 listen_threads = [] # Threads to listen to client threads, 1 thread for each client
 if len(sys.argv) > 1:
     port = int(sys.argv[1])
 else:
     port = 12345  # Default port if user doesn't enter one
-NUM_MACHINES = 2
-
-BUFFER_SIZE = 1024
 
 
-def create_connection(port):
-    print("Connection thread created with port", port)
+def create_connection(my_port):
+    print("Connection thread created with port", my_port)
     tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # For some reason there's a timeout before sockets can be used again, reuseaddr fixes this problem
     tcp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     try:
-        tcp_server_socket.bind(("localhost", port))
+        tcp_server_socket.bind(("localhost", my_port))
         tcp_server_socket.listen(1)
-        print("Tcpserver calling accept on port ", port)
+        print("Tcpserver calling accept on port ", my_port)
         conn, addr = tcp_server_socket.accept()
-        print("Appending to connections the following info: ", connections, addr)
-        connections.append(conn)
+        print("Adding to connections the following info: ", connections, addr)
+        #connections.append(conn)
+        index = my_port - port
+        connections[index] = conn
     finally:
-        print("Closing setup socket (either executed properly or timed out)")
+        print("closing 1 time setup socket")
         tcp_server_socket.close()
 
 
-def listen_for_messages(connection):
+def listen_for_messages(connection, machine_index):
     print("Listening on connection")
     while True:
         data = connection.recv(BUFFER_SIZE)
-        if not data or data == "exit":  # If data is empty, it means client closed connection
-            print("Client closed connection")
-            connection.close()
-            connections.remove(connection)
-            print("Updated connection list:")
+        if not data or data.decode("utf-8") == "exit":  # If data is empty, it means client closed connection
+            print("Clients closed connection")
+            for connection in connections:
+                connection.close()
+            #print("Updated connection list: {}".format(connections))
             break
 
-        print("Received data", data.decode("utf-8"))
-        # Send the data to all the other clients
-        for other_connection in connections:
-            if other_connection != connection:
-                other_connection.send(data)
+        client_messages = data.decode("utf-8")
+        client_message_list = client_messages.split("EOM")
+        #print ("client message list:", client_message_list)
 
+        for i in range(len(client_message_list)-1):
+            client_message = client_message_list[i]
+            client_command = client_message.split(",")[0]
+            client_time = client_message.split(",")[1]
+            client_port = client_message.split(",")[2]
+
+            print("Client message:", client_message)
+
+            #print useful log info
+            print("Client: {} Local time: {} Command: {}".format(client_port, client_time, client_command))
+
+            #construct return message binary
+            message_binary = bytes((client_message + "EOM"), encoding="ascii")
+
+            if (client_command == "like"):
+                time.sleep(5) #makes it easy to create collisions
+                for i in range(len(connections)):
+                    if connections[i] != connection:
+                        mutexes[machine_index].acquire()
+                        try:
+                            connections[i].send(message_binary)
+                        finally:
+                            mutexes[machine_index].release()
+            elif (client_command.split(" ")[0] == "ack"):
+                source_port = client_command.split(" ")[1]
+                #only send it to the correct source port
+                index = int(source_port) - int(port)
+                mutexes[machine_index].acquire()
+                try:
+                    connections[index].send(message_binary)
+                finally:
+                    mutexes[machine_index].release()
+                print("sending {} back ack to {} index {}".format(message_binary.decode("utf-8"),source_port, index))
+        
 
 for i in range(NUM_MACHINES):  # Establish connections to all clients first
     t = Thread(target=create_connection, args=(port+i, ))
@@ -59,7 +100,7 @@ for t in threads:  # Clean up threads
     t.join()
 
 for i in range(NUM_MACHINES):  # Establish a listen thread for all clients
-    t = Thread(target=listen_for_messages, args=(connections[i], ))
+    t = Thread(target=listen_for_messages, args=(connections[i], i, ))
     listen_threads.append(t)
     t.start()
 
