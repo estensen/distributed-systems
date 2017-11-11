a6af4b40df9dc5bf52493a1f46a801e2f2df8611
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-11-10 17:17:14 -0800

    Add client commands

--- paxos/client.py
+++ paxos/client.py
@@ -10,11 +10,29 @@ connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.connect(data_center_addr)
 
 
-while True:
-    user_input = input("Send msg to datacenter: ")
+def send_msg(msg):
     msg = bytes(user_input, encoding="ascii")
     if not msg:
         connection.close()
         print("Closing connection...")
     connection.send(msg)
     print("Message sent to", data_center_addr)
+
+
+def process_user_input(user_input):
+    words = user_input.split(" ")
+    command = words[0]
+    if len(words) > 1:
+        parameter = words[1]
+
+    if command == "show":
+        send_msg("show")
+    elif command == "buy" and parameter.isdigit():
+        send_msg("{},{}".format(command, parameter))
+    else:
+        print("Couldn't recognize the command", user_input)
+
+
+while True:
+    user_input = input("Send msg to datacenter: ")
+    process_user_input(user_input)
