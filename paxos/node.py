deeb329dd29a489cf159247ba4997fafa668c2bc
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-11-14 14:00:50 -0800

    Add Proposer logic

--- paxos/node.py
+++ paxos/node.py
@@ -2,16 +2,69 @@ class Proposer:
     def __init__(self, uid):
         self.uid = uid
         self.leader = False
+
         self.proposal_id = None
         self.proposal_val = None
+        self.next_proposal_num = 1
+
+        self.last_accepted_id = None
 
     def set_proposal(self, val):
         if self.proposal_val = None:
             self.proposal_val = val
 
+    def send_prepare(self, proposal_id):
+        data = ["prepare", proposal_id]
+        send_data_to_all(data)
+
+    def prepare(self):
+        self.proposal_id = (self.next_proposal_id, self.uid)
+        self.next_proposal_num += 1
+        self.recv_promises = set()
+        send_prepare(self.proposal_id)
+
+    def start_election(self):
+        ballot_num = (self.proposal_id, self.uid)
+        data = ["prepare", ballot_num]
+        send_data_to_all(data)
+
+    def recv_promise(self, promise):
+        if promised_id != self.proposal_id:
+            return
+
+        self.recv_promises.add(promise.uid)
+
+        if promise.prev_accepted_id > self.last_accepted_id:
+            # An acceptor has already accepted a val
+            self.proposal_val = promise.prev_accepted_id
+
+        if len(self.recv_promises) > self.quorum_size:
+            data = ["accept", self.proposal_id, self.proposal_val]
+            send_data_to_all(data)
+
+    def accept_leader(self):
+        '''After received ack from majority'''
+        if self.received_val = None:
+            data = ["accept", ballot_num, self.proposal_val]
+            send_data_to_all(data)
+
 
 class Acceptor:
     def __init__(self):
+        self.promised_id = None
+        self.accepted_num = None
+        self.accepted_val = None
+
+    def accept_proposal(uid, proposal_id):
+        data = [uid, proposal_id, self.accepted_num, self.accepted_val]
+        send_data(data)
+
+    def recv_proposal(self, uid, proposal_id):
+        if self.promised_id = None:
+            self.promised_id = proposal_id
+            self.accept_proposal(uid, proposal_id)
+
+    def recv_accept_prepare(self, uid, proposal_id, val):
         pass
 
 
