9b49a2c7601c273f0faca99a1d180525f57c770d
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-11-14 11:58:11 -0800

    Add classes for the roles Proposer, Acceptor and Learner

---
+++ paxos/nodes.py
@@ -0,0 +1,19 @@
+class Proposer:
+    def __init__(self):
+        pass
+
+
+class Acceptor:
+    def __init__(self):
+        pass
+
+
+class Learner:
+    def __init__(self):
+        pass
+
+
+class Node(Proposer, Acceptor, Learner):
+    '''Each node performs all roles'''
+    def __init__(self):
+        pass
