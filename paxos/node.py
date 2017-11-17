d44177d5cd61ad619a0323ebd859cbf415c5f898
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-11-17 11:25:15 -0800

    Move methods for communication to Messenger class

--- paxos/node.py
+++ paxos/node.py
@@ -1,12 +1,15 @@
 class Messenger:
     def send_prepare(self, proposal_id):
-        pass
+        data = ["prepare", proposal_id]
+        send_data_to_all(data)
 
     def send_promise(self, proposer_uid, proposal_id, previous_id, accepted_val):
-        pass
+        data = [propser_uid, proposal_id, previous_id, accepted_val]
+        send_data(data)
 
     def send_accept(self, proposal_id, proposal_val):
-        pass
+        data = ["accept", self.proposal_id, self.proposal_val]
+        send_data_to_all(data)
 
     def send_accepted(self, proposal_id, accepted_val):
         pass
@@ -16,6 +19,7 @@ class Proposer:
     def __init__(self, uid):
         self.uid = uid
         self.leader = False
+        self.messenger = None
 
         self.proposal_id = None
         self.proposal_val = None
@@ -27,17 +31,15 @@ class Proposer:
         if self.proposal_val = None:
             self.proposal_val = val
 
-    def send_prepare(self, proposal_id):
-        data = ["prepare", proposal_id]
-        send_data_to_all(data)
-
     def prepare(self):
         self.proposal_id = (self.next_proposal_id, self.uid)
         self.next_proposal_num += 1
         self.recv_promises = set()
-        send_prepare(self.proposal_id)
+        self.messenger.send_prepare(self.proposal_id)
 
     def start_election(self):
+        # After not recv heartbeats from previous leader and
+        # some random time so everyone doesn't start election at the same time
         ballot_num = (self.proposal_id, self.uid)
         data = ["prepare", ballot_num]
         send_data_to_all(data)
@@ -53,41 +55,33 @@ class Proposer:
             self.proposal_val = promise.prev_accepted_id
 
         if len(self.recv_promises) > self.quorum_size:
-            data = ["accept", self.proposal_id, self.proposal_val]
-            send_data_to_all(data)
-
-    def accept_leader(self):
-        '''After received ack from majority'''
-        if self.received_val = None:
-            data = ["accept", ballot_num, self.proposal_val]
-            send_data_to_all(data)
+            # After received ack from majority
+            self.messenger.send_accept(self.proposal_id, self.proposal_val)
 
 
 class Acceptor:
     def __init__(self):
+        self.messenger = None
+
         self.promised_id = None
         self.accepted_num = None
         self.accepted_val = None
 
-    def accept_proposal(uid, proposal_id):
-        data = [uid, proposal_id, self.accepted_num, self.accepted_val]
-        send_data(data)
-
-    def recv_proposal(self, uid, proposal_id):
+    def recv_prepare(self, proposer_uid, proposal_id):
         if self.promised_id = None:
             self.promised_id = proposal_id
             self.accept_proposal(uid, proposal_id)
 
-    def recv_accept_prepare(self, uid, proposal_id, val):
+    def recv_accept(self, proposer_uid, proposal_id, proposal_val):
         pass
 
 
 class Learner:
     def __init__(self):
-        pass
+        self.messenger = None
 
 
 class Node(Proposer, Acceptor, Learner):
     '''Each node performs all roles'''
-    def __init__(self):
-        pass
+    def __init__(self, messenger):
+        self.messenger = messenger
