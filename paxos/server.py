9b269b6eb725326fce23e03da0ab6563451c02db
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-11-24 15:13:26 -0800

    Impl recv_accept, send_accept, recv_accepts

--- paxos/server.py
+++ paxos/server.py
@@ -37,13 +37,14 @@ class Server:
     def send_prepare(self):
         self.proposal_id = (self.next_proposal_num, self.uid)
         self.next_proposal_num += 1
-        self.recv_promises = set()
+        self.recv_promises_uid = set()
+        self.recv_accepts_uid = set()
         data = "prepare,{},{}".format(self.proposal_id[0], self.proposal_id[1])
         self.send_data_to_all(data)
 
     def recv_prepare(self, proposal_num, proposer_id):
         proposal_id = (proposal_num, proposer_id)
-        if self.promised_id == None or proposal_id > self.promised_id:
+        if self.promised_id == None or proposal_id >= self.promised_id:
             # Higher than current promise
             self.promised_id = proposal_id
 
@@ -53,7 +54,8 @@ class Server:
                 self.uid,
                 self.last_accepted_num,
                 self.last_accepted_proposer_id,
-                self.last_accepted_val)
+                self.last_accepted_val
+            )
 
             from_addr = ("localhost", int(proposal_id[1]))
             self.send_data(promise_msg, from_addr)
@@ -61,20 +63,42 @@ class Server:
 
     def recv_promise(self, proposal_num, proposer_id, from_uid, \
         last_accepted_num, last_accepted_proposer_id, last_accepted_val):
-        # JSON?
             if last_accepted_num != "None" and last_accepted_proposer_id != "None":
                 if self.proposal_id < (int(last_accepted_num), int(last_accepted_proposer_id)):
                     # And > id
                     # An acceptor has already accepted a val
                     self.proposal_val = last_accepted_val
 
-            self.recv_promises.add(from_uid)
-            if len(self.recv_promises) >= QUORUM_SIZE:
-                self.send_accept()
+            self.recv_promises_uid.add(from_uid)
+            if len(self.recv_promises_uid) >= QUORUM_SIZE:
+                self.send_accepts()
 
-    def send_accept(self):
+    def send_accepts(self):
         data = "accept,{},{},{}".format(self.proposal_id[0], self.proposal_id[1], self.proposal_val)
         self.send_data_to_all(data)
+
+    def recv_accept(self, proposal_num, proposer_id, proposal_val):
+        proposal_id = (proposal_num, proposer_id)
+        if proposal_id >= self.promised_id:
+            # Accept proposal
+            self.last_accepted_num = proposal_num
+            self.last_accepted_proposer_id = proposer_id
+            self.last_accepted_val = proposal_val
+
+            self.send_accept()
+
+    def send_accept(self):
+        accepted_data = "accepted,{},{},{},{}".format(self.last_accepted_num, \
+            self.last_accepted_proposer_id, self.uid, self.last_accepted_val)
+        to_addr = ("localhost", int(self.last_accepted_proposer_id))
+        self.send_data(accepted_data, to_addr)
+
+    def recv_accepts(self, proposal_num, proposer_id, from_uid, proposal_val):
+        self.recv_accepts_uid.add(from_uid)
+
+        if len(self.recv_accepts_uid) >= QUORUM_SIZE:
+            self.leader = True
+            print("I am leader")
 
     def send_data(self, data, addr):
         msg = bytes(data, encoding="ascii")
@@ -114,14 +138,11 @@ class Server:
                 #accept_msg = "accept"
                 #self.send_data(accept_msg, addr)
             elif command == "accept":
-                accepted_msg = "accepted"
-                self.send_data(accepted_msg, addr)
+                proposal_num, proposer_id, proposal_val = msg_list[1:]
+                self.recv_accept(proposal_num, proposer_id, proposal_val)
             elif command == "accepted":
-                print("Received accepted")
-                #self.recv_promises.add(addr)
-                #if len(self.recv_promises) >= QUORUM_SIZE:
-                #    self.leader = True
-                #    print("I am leader")
+                self.leader = True
+                print("I am leader")
             elif command == "heartbeat":
                 self.last_recv_heartbeat = time()
 
